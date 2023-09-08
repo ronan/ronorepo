9828498b4aeccbfe1abcbf9583c734dc6ef749d4
Ronan Dowling <ronan@dowlings.me>
2023-09-08 17:54:19 +0000

    Dayboard: switch do json API

--- dayboard/dayboard.py
+++ dayboard/dayboard.py
@@ -1,16 +1,18 @@
-# watch 'date "+%H %M" --date="5 hours ago" | xargs python dayboard.py'
+# dayboard.py
+# usage: 
+#   python dayboard.py [hours] [minutes]
+# Run:
+#   watch 'date "+%H %M" --date="5 hours ago" | xargs python dayboard.py'
 
-import socket
 import math
 import sys
+import requests
 
 hour = int(sys.argv[1])
 minute = int(sys.argv[2])
+print(f"Time: {hour}:{minute}")
 
-print(f"{hour}:{minute}")
-
-UDP_IP = socket.gethostbyname("dayboard.local")
-UDP_PORT = 19446
+WLED_JSON_API_URL = "http://dayboard.local/json"
 
 DAY_START = 6                           # 6am
 NUM_HOURS = 18                          # 6am-midnight
@@ -18,21 +20,34 @@ NUM_HOURS = 18                          # 6am-midnight
 LEDS_PER_HOUR = 6                       # ~30" of 144/m led strip
 DAY_LEDS = NUM_HOURS * LEDS_PER_HOUR    # 108
 
-OFF = [0,0,0]
-PAST = [0, 0, 0]
-FUTURE = [60, 60, 30]
-PRESENT = [0, 255, 0]
+OFF     = [0,0,0]
+PAST    = [0, 0, 0]
+FUTURE  = [100, 100, 70]
+PRESENT = [255, 255, 255]
 
-if hour > DAY_START:
-    dot = (hour - DAY_START) * LEDS_PER_HOUR
-    dot += math.ceil((minute / 60) * LEDS_PER_HOUR)
+if hour >= DAY_START:
+    top = (hour - DAY_START) * LEDS_PER_HOUR
+    top += math.ceil((minute / 60) * LEDS_PER_HOUR)
+    tail = (DAY_LEDS-top)
 
-    print(f"{dot} x {(DAY_LEDS-dot)}")
-    m = (PAST * dot) + PRESENT + (FUTURE * (DAY_LEDS-dot))
+    print(f"LEDs: {top} x {tail}")
+    payload = {
+        "on": True,
+        "bri": 100,
+        "live": False,
+        "seg": [
+            {
+                "start": 10,
+                "len": DAY_LEDS,
+                "col": [255,255,255],
+                "i": [PAST]*top + [PRESENT] + [FUTURE]*tail
+            },
+        ]
+    }
+
+    # from pprint import pprint
+    # pprint(payload)
+    response = requests.post(WLED_JSON_API_URL, json=payload)
 else:
-    m = OFF * (DAY_LEDS + 1)
+    response = requests.post(WLED_JSON_API_URL, json={"on": False})
     print("Night mode")
-
-print(f'UDP target: {UDP_IP}:{UDP_PORT}')
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(m), (UDP_IP, UDP_PORT))
