3ad50c5876c766e701a277a68b4c9bcf4ba76361
Ronan Dowling <ronan@dowlings.me>
2023-09-08 21:05:43 -0500

    simpler arg handling, day of week

--- dayboard/dayboard.py
+++ dayboard/dayboard.py
@@ -12,41 +12,41 @@ import time
 
 import requests
 
-DAY_START           = 6                     # 6am
-NUM_HOURS           = 18                    # 6am-midnight
-LEDS_PER_HOUR       = 6                     # ~30" of 144/m led strip. 6 leds x 18 hrs = 108 leds per day
+DAY_START           =   6                     # 6am
+NUM_HOURS           =   18                    # 6am-midnight
+LEDS_PER_HOUR       =   6                     # ~30" of 144/m led strip. 6 leds x 18 hrs = 108 leds per day
 
-LED_START_INDEX     = 10
-GEOCODE_CACHE_TIME  = 60 * 60 * 24 * 7      # Re-check the location every 7 days
-WEATHER_CACHE_TIME  = 60 * 60 * 2           # Re-check the weather every 2 hours
+LED_START_INDEX     =   10
+GEOCODE_CACHE_TIME  =   60 * 60 * 24 * 7      # Re-check the location every 7 days
+WEATHER_CACHE_TIME  =   60 * 60 * 2           # Re-check the weather every 2 hours
 
-WLED_JSON_API_URL   = "http://dayboard.local/json"
+WLED_JSON_API_URL   =   "http://dayboard.local/json"
 
-PAST                = [0, 0, 0]
-FUTURE              = [100, 100, 70]
+BRIGHTNESS          =   50 # 0-255
 
-BRIGHTNESS          = 50 # 0-255
 
-TEMPS   = [
-            [   1,  27, 105],
-            [   1,  40, 127],
-            [   1,  70, 168],
-            [   1,  92, 197],
-            [   1, 119, 221],
-            [   3, 130, 151],
-            [  23, 156, 149],
-            [  67, 182, 112],
-            [ 121, 201,  52],
-            [ 142, 203,  11],
-            [ 224, 223,   1],
-            [ 252, 187,   2],
-            [ 247, 147,   1],
-            [ 237,  87,   1],
-            [ 229,  43,   1],
-            [ 171,   2,   2],
-            [  80,   3,   3],
-            [  80,   3,   3],
-]
+COLOR_BG            =   [   0,   0,  0]
+COLOR_PAST          =   [   0,   0,  0]
+FUTURE              =   [ 100, 100, 70]
+DAY_MARKER          =   [ 255,   0,  0]
+COLOR_TEMPS         =  [[   1,  27, 105],
+                        [   1,  40, 127],
+                        [   1,  70, 168],
+                        [   1,  92, 197],
+                        [   1, 119, 221],
+                        [   3, 130, 151],
+                        [  23, 156, 149],
+                        [  67, 182, 112],
+                        [ 121, 201,  52],
+                        [ 142, 203,  11],
+                        [ 224, 223,   1],
+                        [ 252, 187,   2],
+                        [ 247, 147,   1],
+                        [ 237,  87,   1],
+                        [ 229,  43,   1],
+                        [ 171,   2,   2],
+                        [  80,   3,   3],
+                        [  80,   3,   3]]
 
 def pp(x):
     from pprint import pprint
@@ -97,29 +97,30 @@ def get_colors():
 
         for i in range(NUM_HOURS):
             idx = math.floor((temps['hourly']['apparent_temperature'][i + DAY_START]+ 40) / 10)
-            colors[i*LEDS_PER_HOUR:(i*LEDS_PER_HOUR)+LEDS_PER_HOUR] = [TEMPS[idx]] * LEDS_PER_HOUR
+            colors[i*LEDS_PER_HOUR:(i*LEDS_PER_HOUR)+LEDS_PER_HOUR] = [COLOR_TEMPS[idx]] * LEDS_PER_HOUR
 
         cache('colors', colors)
     return colors
 
-def draw_board(hour, minute):
+def render(hour, minute, day=-1):
     print(f"Drawing Board: {hour}:{minute}")
 
     if hour >= DAY_START:
         colors = get_colors()
-
         start = int(((hour - DAY_START) + (minute / 60)) * LEDS_PER_HOUR)
+        leds = [COLOR_BG]*10 + [COLOR_PAST]*start + colors[start:]
 
-        leds = [PAST]*start + colors[start:]
-
-        # Print the colorful little dots to stdout for no reason except it's fun
-        print("".join([f'\033[38;2;{led[0]};{led[1]};{led[2]}m•\033[0m' for led in leds]))
+        if day:
+            leds[day] = DAY_MARKER
 
         payload = {
             "on": True,
             "bri": BRIGHTNESS,
-            "seg": [{ "start": LED_START_INDEX, "i": leds },]
+            "seg": [{ "i": leds },]
         }
+
+        # Print the colorful little dots to stdout for no reason except it's fun
+        print("".join([f'\033[38;2;{led[0]};{led[1]};{led[2]}m•\033[0m' for led in leds]))
     else:
         payload = {"on": False}
         print("Night mode")
@@ -127,10 +128,11 @@ def draw_board(hour, minute):
     if payload:
         requests.post(WLED_JSON_API_URL, json=payload)
 
-if len(sys.argv) >= 3:
-    hour = int(sys.argv[1])
-    minute = int(sys.argv[2])
-    draw_board(hour, minute)
-else:
-    t = time.localtime()
-    draw_board(t.tm_hour, t.tm_min)
+
+t    = time.localtime()
+args = dict(enumerate(sys.argv))
+render( 
+    int(args.get(1, t.tm_hour)), 
+    int(args.get(2, t.tm_hour)),
+    int(args.get(3, t.tm_wday))
+)
