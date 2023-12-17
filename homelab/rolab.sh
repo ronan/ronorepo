67acaa72204e8615f62e79d33f5949e5c6a3e482
Ronan Dowling <ronan@dowlings.me>
2023-12-17 15:56:24 +0000

    Remove mdns

--- homelab/rolab.sh
+++ homelab/rolab.sh
@@ -4,12 +4,12 @@ ARCH="amd64";
 DIST="debian.12~bookworm_$ARCH"
 BASE="https://download.docker.com/linux/debian/dists/bookworm/pool/stable/$ARCH";
 
-$PKGS=(
-  "containerd.io_1.6.26-1_$ARCH.deb"
-  "docker-ce_24.0.7-1~$DIST.deb"
-  "docker-ce-cli_24.0.7-1~$DIST.deb"
-  "docker-buildx-plugin_0.11.2-1~$DIST.deb"
-  "docker-compose-plugin_2.21.0-1~$DIST.deb"
+PKGS=( \
+  "containerd.io_1.6.26-1_$ARCH.deb" \
+  "docker-ce_24.0.7-1~$DIST.deb" \
+  "docker-ce-cli_24.0.7-1~$DIST.deb" \
+  "docker-buildx-plugin_0.11.2-1~$DIST.deb" \
+  "docker-compose-plugin_2.21.0-1~$DIST.deb" \
 )
 
 for pkg in $PKGS; do
